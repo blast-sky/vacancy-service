ace669aa22a2e7840be06536056e5a34d3aab556
blast-sky <strogancev2020@gmail.com>
2023-06-28 20:20:59 +0300

    add integration test configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
+	id("org.unbroken-dome.test-sets") version "4.0.0"
 	id("org.springframework.boot") version "3.1.1"
 	id("io.spring.dependency-management") version "1.1.0"
 	kotlin("jvm") version "1.8.22"
@@ -25,6 +26,24 @@ repositories {
 	mavenCentral()
 }
 
+springBoot {
+	buildInfo()
+}
+
+tasks.bootJar {
+	archiveFileName.set("app.jar")
+}
+
+
+testSets {
+	create("integrationTest")
+}
+
+val integrationTestImplementation: Configuration by configurations.getting {
+	extendsFrom(configurations.implementation.get())
+}
+
+
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-amqp")
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
@@ -34,6 +53,11 @@ dependencies {
 
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.1.0")
+	implementation("org.springframework.data:spring-data-redis")
+
+	implementation("io.github.oshai:kotlin-logging-jvm:4.0.0")
+	implementation("redis.clients:jedis")
 
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
 
@@ -43,6 +67,13 @@ dependencies {
 
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.springframework.amqp:spring-rabbit-test")
+
+	integrationTestImplementation("org.junit.jupiter:junit-jupiter")
+	integrationTestImplementation("org.springframework.boot:spring-boot-starter-test") {
+		exclude(group = "org.mockito", module = "mockito-core")
+	}
+	integrationTestImplementation("org.springframework.amqp:spring-rabbit-test")
+	integrationTestImplementation("com.ninja-squad:springmockk:3.0.1")
 }
 
 tasks.withType<KotlinCompile> {
