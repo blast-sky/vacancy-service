df4498da4ebe77763b39707247a10e444f05c104
blast-sky <strogancev2020@gmail.com>
2023-07-14 20:44:12 +0300

    exclude configuration from tests

--- src/main/kotlin/com/astrog/vacancyservice/configuration/CheckRedisConnectionListener.kt
+++ src/main/kotlin/com/astrog/vacancyservice/configuration/CheckRedisConnectionListener.kt
@@ -1,10 +1,12 @@
 package com.astrog.vacancyservice.configuration
 
+import org.springframework.context.annotation.Profile
 import org.springframework.context.event.ContextRefreshedEvent
 import org.springframework.context.event.EventListener
 import org.springframework.data.redis.core.RedisTemplate
 import org.springframework.stereotype.Component
 
+@Profile("!test")
 @Component
 class CheckRedisConnectionListener(
     private val redisTemplate: RedisTemplate<String, String>,
