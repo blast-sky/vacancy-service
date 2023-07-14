a4d90bc83d00334a1986895045e5b46a095f5d65
blast-sky <strogancev2020@gmail.com>
2023-07-15 00:00:18 +0300

    tune model nullability

--- src/main/kotlin/com/astrog/vacancyservice/model/dto/LogoUrls.kt
+++ src/main/kotlin/com/astrog/vacancyservice/model/dto/LogoUrls.kt
@@ -4,10 +4,10 @@ import com.fasterxml.jackson.annotation.JsonProperty
 
 data class LogoUrls(
     @JsonProperty("90")
-    val nineteen: String,
+    val nineteen: String?,
 
     @JsonProperty("240")
-    val twoHundred: String,
+    val twoHundred: String?,
 
-    val original: String,
+    val original: String?,
 )
