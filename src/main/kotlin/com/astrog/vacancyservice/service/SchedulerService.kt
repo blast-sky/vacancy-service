6f0953b9abd94ff63ce9063a9f9430d861539006
blast-sky <strogancev2020@gmail.com>
2023-07-15 16:04:17 +0300

    exclude from test

--- src/main/kotlin/com/astrog/vacancyservice/service/SchedulerService.kt
+++ src/main/kotlin/com/astrog/vacancyservice/service/SchedulerService.kt
@@ -1,14 +1,16 @@
 package com.astrog.vacancyservice.service
 
+import org.springframework.context.annotation.Profile
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Service
 
+@Profile("!test")
 @Service
 class SchedulerService(
     private val pullerService: PullerService
 ) {
 
-    @Scheduled(cron = "0 0/30 * * * * ?")
+    @Scheduled(cron = "0 0/30 * * * *")
     fun pullVacancies() {
         pullerService.pullLastPages()
     }
